b9575a4cec261489d0758f6f3b6c8356ac4ea23d
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-12-16 18:25:26 +0300

    complete task of 5th week

--- 5 week/index.js
+++ 5 week/index.js
@@ -1,12 +1,19 @@
 module.exports = {
-
+    events: {},
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
+        var target = (this.events.hasOwnProperty(event)) ? this.events[event] : this.events[event] = [];
 
+        target.push({
+            subscriber: subscriber,
+            handler: handler.bind(subscriber)
+        });
+
+        return this;
     },
 
     /**
@@ -14,13 +21,24 @@ module.exports = {
      * @param {Object} subscriber
      */
     off: function (event, subscriber) {
+        if (this.events.hasOwnProperty(event)) {
+            this.events[event] = this.events[event].filter(function (item) {
+                return item.subscriber !== subscriber;
+            });
+        }
 
+        return this;
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
-
+        if (this.events.hasOwnProperty(event)) {
+            this.events[event].forEach(function (element) {
+                element.handler();
+            })
+        }
+        return this;
     }
 };
