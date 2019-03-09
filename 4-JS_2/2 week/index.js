32c45313809a3603f7a8e527990b2807c8730c92
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-03-09 21:05:41 +0300

    prototype collection functions

--- 4-JS_2/2 week/index.js
+++ 4-JS_2/2 week/index.js
@@ -8,11 +8,25 @@ function Collection() {}
 
 
 // Методы коллекции
+// возвращает массив элементов коллекции
 Collection.prototype.values = function () {};
-// другие методы
 
+// возвращает элемент с определенной позиции
+Collection.prototype.at = function (el) {};
+
+// добавляет элемент в коллекцию
+Collection.prototype.append = function (el) {};
+
+// удаляет элемент с переданной позиции
+Collection.prototype.removeAt = function (pos) {
+    return true;
+};
 
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function () {};
+Collection.from = function (arr) {
+    // перевести массив arr в объект
+    return arr;
+};
+
