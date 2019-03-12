d827f33397ce723f67928f19a4a273bc6647fa6b
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-03-12 22:06:35 +0300

    'fix'

--- 4-JS_2/2 week/index.js
+++ 4-JS_2/2 week/index.js
@@ -1,32 +1,72 @@
 module.exports = Collection;
 
+
 /**
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
+function Collection() {
+    this.acc = [];
+}
 
 
 // Методы коллекции
 // возвращает массив элементов коллекции
-Collection.prototype.values = function () {};
+Collection.prototype.values = function () {
+    if (this.acc instanceof Array) {
+        return this.acc;
+    } else {
+        throw new Error('Должен быть передан массив');
+    }
+};
+
+// возвращает массив элементов коллекции
+Collection.prototype.count = function () {
+    return this.acc.length;
+};
 
 // возвращает элемент с определенной позиции
 Collection.prototype.at = function (el) {};
 
 // добавляет элемент в коллекцию
-Collection.prototype.append = function (el) {};
+Collection.prototype.append = function (el) {
+    this.acc.push(el);
+
+    return this.acc;
+};
 
 // удаляет элемент с переданной позиции
 Collection.prototype.removeAt = function (pos) {
-    return true;
+    // if () {
+    //     return true;
+    // }
+    // return false;
 };
 
 /**
  * Создание коллекции из массива значений
  */
+
+
 Collection.from = function (arr) {
-    // перевести массив arr в объект
-    return arr;
+    if (arr instanceof Array) {
+        if (this.acc != undefined) {
+            return this.acc.concat(arr);
+        } else {
+            this.acc = arr;
+        }
+    } else {
+        throw new Error('Должен быть передан массив');
+    }
 };
+//
+// var numbers = new Collection;
+var letters = Collection.from(['a', 'b', 'c']);
+// console.info(numbers);
+// debugger
+// var numbers = Collection.from(['a', 'b', 'c']);
+
+console.info(letters.values());
+console.info(letters.count());
+letters.append('d');
 
