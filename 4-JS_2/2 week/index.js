9e6c66bbd10b18b20f9605436ab2f625f11ba3bb
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-03-17 19:56:22 +0300

    done Collection

--- 4-JS_2/2 week/index.js
+++ 4-JS_2/2 week/index.js
@@ -16,7 +16,8 @@ Collection.prototype.values = function () {
     if (this.acc instanceof Array) {
         return this.acc;
     } else {
-        throw new Error('Должен быть передан массив');
+        return this;
+        // throw new Error('Должен быть передан массив');
     }
 };
 
@@ -26,21 +27,38 @@ Collection.prototype.count = function () {
 };
 
 // возвращает элемент с определенной позиции
-Collection.prototype.at = function (el) {};
+Collection.prototype.at = function (el) {
+    if ((el <= 0) || typeof el !== 'number' || el > this.acc.length) {
+        return null;
+    } else {
+        var pos = el - 1;
+        return this.acc[pos];
+    }
+};
 
 // добавляет элемент в коллекцию
-Collection.prototype.append = function (el) {
-    this.acc.push(el);
-
-    return this.acc;
+Collection.prototype.append = function () {
+    if (arguments.length > 0 ){
+        for (var i = 0; i < arguments.length; i++){
+            if (arguments[i] instanceof Collection){
+                this.acc = this.acc.concat(Collection.prototype.values.call(arguments[i]));
+            } else if (Array.isArray(arguments[i])){
+                this.acc = this.acc.concat(arguments[i]);
+            } else {
+                this.acc.push(arguments[i]);
+            }
+        }
+    }
 };
 
 // удаляет элемент с переданной позиции
 Collection.prototype.removeAt = function (pos) {
-    // if () {
-    //     return true;
-    // }
-    // return false;
+    if ((pos <= 0)||(pos > this.acc.length)||(typeof pos !== 'number')){
+        return false;
+    } else {
+        this.acc.splice((pos - 1), 1);
+        return true;
+    }
 };
 
 /**
@@ -49,24 +67,15 @@ Collection.prototype.removeAt = function (pos) {
 
 
 Collection.from = function (arr) {
+    var collection = new Collection();
     if (arr instanceof Array) {
-        if (this.acc != undefined) {
-            return this.acc.concat(arr);
-        } else {
-            this.acc = arr;
-        }
-    } else {
-        throw new Error('Должен быть передан массив');
+        collection.acc = collection.acc.concat(Collection.prototype.values.call(arr));
     }
-};
-//
-// var numbers = new Collection;
-var letters = Collection.from(['a', 'b', 'c']);
-// console.info(numbers);
-// debugger
-// var numbers = Collection.from(['a', 'b', 'c']);
 
-console.info(letters.values());
-console.info(letters.count());
-letters.append('d');
+    return collection;
+};
+
+
+
+
 
