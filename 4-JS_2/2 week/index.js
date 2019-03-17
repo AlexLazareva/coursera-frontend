ffeb499e8e9d1bcd268478d623d9d45cd3a8c5aa
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-03-17 20:33:06 +0300

    fix Collection

--- 4-JS_2/2 week/index.js
+++ 4-JS_2/2 week/index.js
@@ -6,15 +6,15 @@ module.exports = Collection;
  * @constructor
  */
 function Collection() {
-    this.acc = [];
+    this._acc = [];
 }
 
 
 // Методы коллекции
 // возвращает массив элементов коллекции
 Collection.prototype.values = function () {
-    if (this.acc instanceof Array) {
-        return this.acc;
+    if (this._acc instanceof Array) {
+        return this._acc;
     } else {
         return this;
         // throw new Error('Должен быть передан массив');
@@ -23,16 +23,16 @@ Collection.prototype.values = function () {
 
 // возвращает массив элементов коллекции
 Collection.prototype.count = function () {
-    return this.acc.length;
+    return this._acc.length;
 };
 
 // возвращает элемент с определенной позиции
 Collection.prototype.at = function (el) {
-    if ((el <= 0) || typeof el !== 'number' || el > this.acc.length) {
+    if ((el <= 0) || typeof el !== 'number' || el > this._acc.length) {
         return null;
     } else {
         var pos = el - 1;
-        return this.acc[pos];
+        return this._acc[pos];
     }
 };
 
@@ -41,11 +41,11 @@ Collection.prototype.append = function () {
     if (arguments.length > 0 ){
         for (var i = 0; i < arguments.length; i++){
             if (arguments[i] instanceof Collection){
-                this.acc = this.acc.concat(Collection.prototype.values.call(arguments[i]));
+                this._acc = this._acc.concat(Collection.prototype.values.call(arguments[i]));
             } else if (Array.isArray(arguments[i])){
-                this.acc = this.acc.concat(arguments[i]);
+                this._acc = this._acc.concat(arguments[i]);
             } else {
-                this.acc.push(arguments[i]);
+                this._acc.push(arguments[i]);
             }
         }
     }
@@ -53,10 +53,10 @@ Collection.prototype.append = function () {
 
 // удаляет элемент с переданной позиции
 Collection.prototype.removeAt = function (pos) {
-    if ((pos <= 0)||(pos > this.acc.length)||(typeof pos !== 'number')){
+    if ((pos <= 0)||(pos > this._acc.length)||(typeof pos !== 'number')){
         return false;
     } else {
-        this.acc.splice((pos - 1), 1);
+        this._acc.splice((pos - 1), 1);
         return true;
     }
 };
@@ -69,7 +69,7 @@ Collection.prototype.removeAt = function (pos) {
 Collection.from = function (arr) {
     var collection = new Collection();
     if (arr instanceof Array) {
-        collection.acc = collection.acc.concat(Collection.prototype.values.call(arr));
+        collection._acc = collection._acc.concat(Collection.prototype.values.call(arr));
     }
 
     return collection;
