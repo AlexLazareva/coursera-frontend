cba6ef0bea51fd773ef84682e8778542b321172e
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-03-23 22:04:07 +0300

    add promise

--- 4-JS_2/3 week/index.js
+++ 4-JS_2/3 week/index.js
@@ -3,5 +3,20 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
+    var promises = [];
 
+    operations.forEach(operation => {
+        let promise = new Promise((resolve, reject) => {
+
+
+        });
+        promises.push(promise);
+    });
+
+    Promise.all(promises)
+        .then(callback => {
+            callback();
+        }, error => {
+            console.log(error);
+        });
 };
