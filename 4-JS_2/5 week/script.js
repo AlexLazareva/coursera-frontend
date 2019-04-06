1c8a00e9c4e81a07377334eeed6bfd3f4139e94c
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-04-06 22:01:25 +0300

    add showError and resetError

--- 4-JS_2/5 week/script.js
+++ 4-JS_2/5 week/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // Код валидации формы
+
+function showError(container, errorMessage) {
+    container.className = 'form_invalid';
+    var messageElem = document.createElement('span');
+    messageElem.className = 'form__error-msg';
+    messageElem.innerHTML = errorMessage;
+    container.appendChild(messageElem);
+}
+
+function resetError(container) {
+    container.className = '';
+    if (container.lastChild.className == "form_invalid") {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function validateForm(obj) {
+    console.log(obj);
+    return obj;
+}
