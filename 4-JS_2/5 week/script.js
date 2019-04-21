957280f6dab96411594cdf2bce7424d339b62098
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-04-21 22:33:17 +0300

    add validate to data attributes

--- 4-JS_2/5 week/script.js
+++ 4-JS_2/5 week/script.js
@@ -2,7 +2,7 @@
 
 // Код валидации формы
 
-function showError(container, formId) {
+function showFormError(container, formId) {
     formId.classList.add(container.formInvalidClass);
 }
 
@@ -10,24 +10,57 @@ function resetError(container, formId) {
     formId.classList.remove(container.formInvalidClass);
 }
 
+function validForm(container, formId) {
+    resetError(container, formId);
+    formId.classList.add(container.formValidClass);
+}
+
+function checkInput(el, container) {
+    switch (el.dataset.validator) {
+        case "number":
+            if (+el.value > el.dataset.validatorMax) {
+                el.classList.add(container.inputErrorClass);
+            } else {
+                el.classList.remove(container.inputErrorClass);
+            }
+            break;
+        case "letters":
+            if (/^[a-zA-Zа-яА-Я]+$/.test(el.value)) {
+                el.classList.remove(container.inputErrorClass);
+            } else {
+                el.classList.add(container.inputErrorClass);
+            }
+            break;
+        case "regexp":
+            console.info(el.value);
+            break;
+        default:
+            validForm(container, container.formId);
+    }
+}
+
 function validateForm(obj) {
     var form = document.getElementById(obj.formId);
     var elems = form.elements;
 
-    form.onsubmit = function () {
+    function checkSubmit() {
         for (var el in elems) {
             console.log(elems[el].value);
-            if(!elems[el].value) {
-                showError(obj, form);
+            if (!elems[el].value) {
+                showFormError(obj, form);
                 event.preventDefault();
+            } else {
+                console.info('Без ошибок!');
             }
         }
-        // if (!elems.name.value) {
-        //     showError(obj, form);
-        //     event.preventDefault();
-        // }
-
-        console.log('checked!', event);
-    };
+    }
+    form.addEventListener('submit', checkSubmit);
+    form.addEventListener('blur', function(event) {
+        if (event.target.tagName === "INPUT")
+        checkInput(event.target, obj);
+    }, true);
     return obj;
 }
+
+
+
